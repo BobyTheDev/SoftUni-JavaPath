d593f50708ed6b2a29efb3dcf3a238cdb4e6322a
Boby <boby2014@abv.bg>
2023-09-25 23:13:38 +0300

    Create Program.java

---
+++ 01. Programming Basics with Java/01.firstStepsInCoding/05. Greeting by Name/Program.java
@@ -0,0 +1,11 @@
+import java.io.Console;
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
