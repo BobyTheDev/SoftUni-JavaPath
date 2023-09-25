a8b449021f78c97f9c0320b237706ccf60e6b3b9
Boby <boby2014@abv.bg>
2023-09-25 22:52:56 +0300

    Create Program.java

---
+++ 01. Programming Basics with Java/01.firstStepsInCoding/02. num1To10/Program.java
@@ -0,0 +1,10 @@
+import java.io.Console;
+
+public class Demo {
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 10; i++) {
+        System.out.println(i);
+        }
+    }
+}
